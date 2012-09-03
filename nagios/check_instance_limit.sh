16019d00b7dab77ee9b62cc8b28c8249d804f0c8
Danilo Perogil <dperogil@gmail.com>
2012-09-03 14:35:58 -0300

    Alterando script que checa o numero de instancias

--- nagios/check_instance_limit.sh
+++ nagios/check_instance_limit.sh
@@ -12,16 +12,13 @@ NUM_INSTANCE_POOL=$($NOVA list | egrep -v -- "(ID|--)" | wc -l)
 NUM_INSTANCE_CONF=$(grep quota_instances $CONF | awk -F '--quota_instances=' '{print $2}')
 LAST_LOG=$(grep -i InstanceLimitExceeded "$LOG" | tail -n1 | awk '{print $3}')
 DATE=$(date +%d)
-LIMIT=$((NUM_INSTANCE_CONF-2))
+LIMIT=$((NUM_INSTANCE_CONF-5))
 
 if [ "$NUM_INSTANCE_POOL" -eq "$NUM_INSTANCE_CONF" ]; then
 	if [ "$LAST_LOG" -eq "$DATE" ]; then
 		printf "Tentativa de criar instancia falhou!! Numero de instancias no Controller chegou ao limite: $NUM_INSTANCE_CONF\n"
 		exit 0
 	fi
-else
-	printf "Numero de instancias no Controller chegou ao limite: $NUM_INSTANCE_CONF\n"
-	exit 1
 fi
 
 if [ "$NUM_INSTANCE_POOL" -ge "$LIMIT" ]; then
