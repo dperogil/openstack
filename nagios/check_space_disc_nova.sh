bbfd932a05dc164ba48315b2e15a384a5f7c11c7
Danilo Perogil <dperogil@gmail.com>
2012-09-03 14:46:05 -0300

    Alterando script que checa o espaco disponivel no NOVA

--- nagios/check_space_disc_nova.sh
+++ nagios/check_space_disc_nova.sh
@@ -8,7 +8,7 @@
 # Variaveis de interacao com o nagios, no cadastro do check no nagios definir !60!100 ou qualquer valor
 DISK_ALERT_CRITICAL="$1"
 DISK_ALERT_WARNING="$2"
-
+NOW=$(date +%d)
 
 CHECK() {
 
@@ -17,16 +17,19 @@ CHECK() {
 	DISK_AVALIABLE=$(egrep -i '(Total disk space)' $LOG | tail -n1 | awk -F "Total disk space =" '{print $2}' | awk '{print $1}')
 	DISK_USAGE=$(egrep -i '(Total VM disk space)' $LOG | tail -n1 | awk -F "Total VM disk space =" '{print $2}' | awk '{print $1}')
 	COUNT=$(($DISK_AVALIABLE-$DISK_USAGE))
+	DATE=$(egrep -i '(Total disk space)' /var/log/nova/nova-scheduler.log | tail -n1 | awk '{print $1}' | awk -F '-' '{print $3}' | xargs)
 
-	if [ "$COUNT" -le "0" ]; then
-		printf "Estouro de disco do NOVA, sem chance de criar instancias!!\n"
-		exit 0
-	elif [ "$COUNT" -le "$DISK_ALERT_CRITICAL" ] ; then
-		printf "Espaco em disco menor que: $COUNT GB\n"
-		exit 0
-	elif [ "$COUNT" -le "$DISK_ALERT_WARNING" ]; then
-		printf "Espaco em disco menor que: $COUNT GB\n"
-		exit 1
+	if [ "$DATE" -eq "$NOW" ]; then
+		if [ "$COUNT" -le "0" ]; then
+			printf "Estouro de disco do NOVA, sem chance de criar instancias!!\n"
+			exit 0
+		elif [ "$COUNT" -le "$DISK_ALERT_CRITICAL" ] ; then
+			printf "Espaco em disco menor que: $COUNT GB\n"
+			exit 0
+		elif [ "$COUNT" -le "$DISK_ALERT_WARNING" ]; then
+			printf "Espaco em disco menor que: $COUNT GB\n"
+			exit 1
+		fi
 	fi
 }
 
